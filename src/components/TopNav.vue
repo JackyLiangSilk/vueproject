8f65a05c1818e690d1afb1bbf0a93fe355c63d62
JackyLiang <jacky.liang@silksoftware.com>
2020-07-23 15:43:47 -0400

    hamburger menu

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,25 +1,31 @@
 <template>
-	<v-app-bar class="top-nav d-flex" 
-	app
-	dark 
-	fixed
-	color="#183B59">
-		<v-toolbar-title>
-			<span class="font-weight-light">Vue</span><span class="text-uppercase font-weight-bold">Project</span>
-		</v-toolbar-title>
-		<v-spacer></v-spacer>
-		<div class="header-link" v-for="(link, index) in headerLinks" :key="index">
-			<router-link 
-			class="pa-4 d-block white--text text-decoration-none text-uppercase font-weight-bold" 
-			:to="link.url">{{link.title}}</router-link>
-		</div>
-    </v-app-bar>
+	<div>
+		<v-app-bar class="top-nav d-flex" 
+		app
+		dark 
+		fixed
+		color="#183B59">
+			<v-app-bar-nav-icon @click="menu = !menu"></v-app-bar-nav-icon>
+			<v-toolbar-title>
+				<img :src="logo" width="30px" class="d-block">
+			</v-toolbar-title>
+		</v-app-bar>
+		<v-navigation-drawer app v-model="menu" color="#183B59">
+			<div class="header-link" v-for="(link, index) in headerLinks" :key="index">
+				<router-link 
+				class="pa-4 d-block white--text text-decoration-none text-uppercase font-weight-bold" 
+				:to="link.url">{{link.title}}</router-link>
+			</div>
+		</v-navigation-drawer>
+	</div>
 </template>
 <script type="text/javascript">
 	export	default{
 		name: 'Logo',
 		data(){
 			return{
+				logo: require('../assets/logo-hello.png'),
+				menu: false,
 				headerLinks: [
 					{
 						url: '/',
