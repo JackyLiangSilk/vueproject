9a5db82200106a678eefce471fc0224852da8151
JackyLiang <jacky.liang@silksoftware.com>
2020-07-23 14:56:59 -0400

    top nav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-app-bar class="top-nav d-flex" 
+	app
+	dark 
+	fixed
+	color="#183B59">
+		<v-toolbar-title>
+			<span class="font-weight-light">Vue</span><span class="text-uppercase font-weight-bold">Project</span>
+		</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<div class="header-link" v-for="(link, index) in headerLinks" :key="index">
+			<router-link 
+			class="pa-4 d-block white--text text-decoration-none text-uppercase font-weight-bold" 
+			:to="link.url">{{link.title}}</router-link>
+		</div>
+    </v-app-bar>
+</template>
+<script type="text/javascript">
+	export	default{
+		name: 'Logo',
+		data(){
+			return{
+				headerLinks: [
+					{
+						url: '/',
+						title: 'Home'
+					},
+					{
+						url: '/about',
+						title: 'About'
+					}
+				]
+			}
+		}
+	}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World Vue"/>
-  </div>
+	<div class="home">
+		<h1>this is home page</h1>
+		<p class="pink--text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo tellus aliquam pulvinar consequat. Nullam tincidunt rutrum lacus, at dapibus lectus. Integer blandit luctus tortor et gravida. Cras dapibus volutpat augue eget tristique. Duis dictum odio magna, id aliquam sapien efficitur ut. Vivamus nisl tortor, sodales quis lacus a, convallis ullamcorper felis. Mauris congue consequat ligula, nec gravida quam mollis eu. Ut in diam placerat, eleifend metus sit amet, elementum dolor. Ut interdum magna id mattis dignissim. Pellentesque faucibus, magna vitae imperdiet tincidunt, quam sem pulvinar tellus, vestibulum consectetur mi turpis et sem. Pellentesque efficitur ornare vehicula. Nulla luctus vehicula magna. Vivamus tellus urna, tincidunt sit amet iaculis non, facilisis ut lacus. Integer eget consectetur urna, venenatis scelerisque ante. Nunc eros neque, accumsan sed vulputate sit amet, tincidunt eu velit.</p>
+	</div>
+    
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
