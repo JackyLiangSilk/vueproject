655065cb837b96046dda7aa7194a76baef404c1d
JackyLiang <jacky.liang@silksoftware.com>
2020-07-31 21:18:48 -0400

    hp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,20 +8,38 @@
 					</v-layout>
 				</v-parallax>
 			</v-flex>
-			<div class="triangle-container d-flex align-center justify-center">
+			<div class="triangle-container d-flex align-center justify-center" style="bottom: 0">
 				<div class="triangle-item pt-left"></div>
 				<div class="triangle-item pt-right"></div>
 			</div>
 		</v-layout>
 		
-		<v-card flat>
+		<v-card flat class="rounded-0">
 			<Tabs />
 		</v-card>
-		<v-card flat>
+		<v-card flat class="rounded-0">
 			<Slider />
 		</v-card>
+		<v-card flat class="rounded-0">
+			<v-card-text class="headline mx py-16 px-4">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere mauris eget mi gravida, quis vehicula tortor ornare. Etiam metus lorem, aliquet et nulla at, commodo auctor felis. Donec ac tincidunt lacus. Curabitur malesuada laoreet mi sit amet condimentum. Proin eget libero egestas nibh ultrices accumsan in at est. Nulla in malesuada nulla, eget rhoncus elit. Fusce quis nisl eget ex commodo ultrices. Sed sed sapien a justo rutrum hendrerit non nec odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin eu pharetra velit, sit amet efficitur lectus.
+			</v-card-text>
+		</v-card>
+		<v-layout row wrap style="position: relative;">
+			<div class="triangle-container d-flex align-center justify-center" style="top: 0">
+				<div class="triangle-item pt-left-rvt"></div>
+				<div class="triangle-item pt-right-rvt"></div>
+				
+			</div>
+			<v-flex xs12 sm12>
+				<v-parallax :src="bg2"  height="800">
+					<v-layout column align-center justify-center>
+						<h1 class="text-md-h3 text-sm-h4 text-uppercase font-weight-bold " style="letter-spacing: 4px !important">Thank You</h1>
+					</v-layout>
+				</v-parallax>
+			</v-flex>
+		</v-layout>
 	</div>
-    
 </template>
 
 <script>
@@ -37,7 +55,8 @@ import Tabs from '@/views/Tabs.vue';
 		},
 		data(){
 			return{
-				bg: require('@/assets/cold-night-time.jpg')
+				bg: require('@/assets/cold-night-time.jpg'),
+				bg2: require('@/assets/adrian-FrSEEdYr418-unsplash.jpg')
 			}
 		}
 	}
@@ -55,10 +74,22 @@ import Tabs from '@/views/Tabs.vue';
 		border-bottom: 100px solid #fff;
 		border-left: 50vw solid transparent;
 	}
+	.pt-left-rvt{
+		width: 0;
+		height: 0;
+		border-top: 100px solid #fff;
+		border-right: 50vw solid transparent;	
+	}
+	.pt-right-rvt{
+		width: 0;
+		height: 0;
+		border-top: 100px solid #fff;
+		border-left: 50vw solid transparent;
+	}
 	.triangle-container{
 		position: absolute;
-		bottom: 0;
 		left: 0;
 		right: 0;
+		z-index: 10;
 	}
 </style>
--- src/views/Slider.vue
+++ src/views/Slider.vue
@@ -1,6 +1,8 @@
 <template>
-	<v-carousel :show-arrows="true"  >
-		<v-carousel-item v-for="item in Gallery" :key="item.index" :src="item.src" transition="fade-transition" reverse-transition="fade-transition"></v-carousel-item>
+	<v-carousel :show-arrows="true" >
+		<v-carousel-item v-for="item in Gallery" :key="item.index" :src="item.src" transition="fade-transition" reverse-transition="fade-transition">
+			
+		</v-carousel-item>
 	</v-carousel>
 </template>
 <script type="text/javascript">
@@ -10,16 +12,16 @@
 		return{
 			Gallery: [
 					{
-						src: require("@/assets/cold-day-time.jpg"),
+						src: require("@/assets/danyu-wang-sR7_ImYvt1Q-unsplash.jpg"),
 					},
 					{
-						src: require("@/assets/cold-night-time.jpg"),
+						src: require("@/assets/dave-hoefler-PDfe7H5GJR0-unsplash.jpg"),
 					},
 					{
-						src: require("@/assets/warm-day-time.jpg")
+						src: require("@/assets/pine-watt-2Hzmz15wGik-unsplash.jpg")
 					},
 					{
-						src: require("@/assets/warm-night-time.jpg")
+						src: require("@/assets/sergey-pesterev-JV78PVf3gGI-unsplash.jpg")
 					}
 				]
 			}
